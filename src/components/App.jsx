06f171df4b9b5f4f6015c56d8b536d4c3d12e3cb
A-Cezara <alexandracezara@yahoo.ro>
2024-10-31 13:20:34 +0200

    settings and basic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
-export const App = () => {
+import React from 'react';
+import Navigation from './Landing/Navigation';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Navigation />
     </div>
   );
-};
+}
---
+++ src/components/Landing/Navigation.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Navigation() {
+  return (
+    <div>
+      <div>Logo</div>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Our services</li>
+        <li>
+          Our Projects <button>v</button>
+        </li>
+        <li>Portofolio</li>
+        <li>
+          Pages <button>v</button>
+        </li>
+        <li>Menu Icon</li>
+      </ul>
+    </div>
+  );
+}
